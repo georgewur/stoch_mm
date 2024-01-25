0d7b4344247204141b71dc14f04928a55ddbc747
Bier <george.bier@wur.nl>
2024-01-25 09:50:51 +0100

    polishing up read_stoch_MT3DMS.R for readability. Plot_stoch_MT3DMS.Rmd: Added time steps to the animation plot a few/single typo

--- read_stoch_MT3DMS.R
+++ read_stoch_MT3DMS.R
@@ -1,23 +1,32 @@
 ###############################################################################
-# R script to calculate the MM from a stochastic run of MT3DMS
+# R script to calculate the Central Mass Moments CMM from a stochastic run of MT3DMS
 # NOT in a markdown because there are currently some issues with markdown/knitr
 # when in a code chunk the directory is changed for reading all *.UCN files
 # G.Bier January, 2024
 ###############################################################################
 rm(list = ls())
-source("MMfunctions.R")
+source("MMfunctions.R")  #file containing several functions to calculate CMM's
 
-# First read all *.UCN files in the STOCHASTIC MT3DMS-FOLDER.
-# For this give the folder name (case sensitive) in de "string"variable `sto_dir`.
-# Next the UCN files will be read in the subsequent folders (001,002,..010)
-sto_dir = "stoch_mm/test_stoch_ass2_MT3DMS"
-cur_dir = getwd()
+# Before running this code assign the proper folder at which the subfolders of
+# the stochastic run is located (i.e. folder numbered 001,002,...010)
+# assign this folder "between quotes" to variable sto_dir.
+# Next the UCN files will be read from the subsequent folders (001,002,..010)
+#
+# Assign the number of time steps which need to be considered.
+# It can be found in the sub panel in the explorer window (lower left on screen)
+# when a solute/species is selected 
+# This number should be assigned to variable NTTS
 
-setwd(paste0(cur_dir,"/",sto_dir))
+sto_dir = "stochastic_bc_MT3DMS"
+NTTS = 147
+
+cur_dir = getwd()  #get currrent directory to go back to this after running
+
+setwd(paste0(cur_dir,"/",sto_dir,"/")) #setting the workin directory for reading the subsequent folders
 print(paste("Folder to read UCN files : ",getwd()))
 sto_subdirs = getwd()
 
-stoch_MM_df = c() #data.frame(colnames("time","stoch.nr","M0","M1x","My1","M2x","M2y","COVxy")) #"time","stoch.nr","M0","M1x","My1","M2x","M2y","COVxy")
+stoch_MM_df = c() # open a data container for the results
 
 ###########calculating the Volume per cell
 delx = 1.0 #10. 
@@ -29,12 +38,13 @@ x_origin = 0.0
 y_origin = 0.0
 ###########calculating the Volume per cell
 
-
+# loop (i) through all 10 subdirectories to read the MT3D001.UCN file
+# and to determine M0, M1x, M1y, M2x and M2y for each time step and subfolder
 for (i in 1: 10)
 {
   if (i < 10 ) setwd(paste0(sto_subdirs,"/00",i))
   if( i == 10) setwd(paste0(sto_subdirs,"/010"))
-  trans_conc = readucn("MT3D001",NLAY = 1,NTTS = 74 )
+  trans_conc = readucn("MT3D001",NLAY = 1,NTTS = nrNTTS )
   ##pts is a vector containing all times steps and required for the calculation of the transient mass moments.
   pts = unique((trans_conc$TIME))
   ## below the dimension of the transport model
@@ -56,34 +66,7 @@ for (i in 1: 10)
   stoch_MM_df = cbind(stoch_MM_df,M0,M1x,M1y,M2x,M2y,COVxy)
 }
 
-setwd(cur_dir)
-stoch_MM_df = cbind(pts,stoch_MM_df)
-stoch_MM_data =  data.frame(stoch_MM_df)
-write.csv(file = "stoch_MM_data.csv",x = stoch_MM_data)
-
-# M0 = Calc_M0(trans_conc)
-# M1x = Calc_M1_x(trans_conc,M0)
-# M1y = Calc_M1_y(trans_conc,M0_time = M0)
-# M2x = Calc_M2_x(trans_conc,M0,M1x)
-# M2y = Calc_M2_y(trans_conc,M0,M1y)
-# COVxy = Calc_M2_xy(trans_conc,M0_time = M0,M1_x_time = M1x,M1_y_time = M1y)
-
-
-
-# library("ellipse")
-# 
-# for (i in 1:length(pts))#requires gifski and ellipse packages!!
-# {
-#   corxy = COVxy[i]/(M2x[i] * M2y[i])
-#   M  = matrix(c(1,corxy,corxy,1),ncol = 2, byrow=TRUE) #the correlation matrix seems to be required for ellipse
-#   
-#   plot(M1x[1:i],M1y[1:i],type="l",xlim=c(2,40),asp=1,col="blue",lwd=2,
-#        main=paste("time=",pts[i]),xlab="x",ylab="y")
-#   points(M1x[i],M1y[i],pch=20,col="blue",cex=2)
-#   polygon(ellipse(M,scale=c(M2x[i],M2y[i]),centre=c(M1x[i],M1y[i]),level=0.68,fill=TRUE),col=rgb(0,0,1,0.2),border=NA)
-#   grid(col="black")
-# }
-
-
-#go back to the original directory
-
+setwd(cur_dir) #go back to the base directory where this file is located
+stoch_MM_df = cbind(pts,stoch_MM_df) #add the time steps to the data
+stoch_MM_data =  data.frame(stoch_MM_df) #create a data.frame from this
+write.csv(file = "stoch_MM_data.csv",x = stoch_MM_data) #write the data file
